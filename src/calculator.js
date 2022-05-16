a81544b13334161eab6575913671f1c0b3cca7ff
Mowalky <mowalky@outlook.com>
2022-05-16 11:38:55 -0500

    updated

---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+export function add(num1,num2) {
+    return num1 + num2;
+}
+
+export default function abc() {
+    console.log('foo')
+    return 'bar';
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import abc, { add } from "./calculator"
+
+alert(add(1, 2));
+abc()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <h1>Hello World!</h1>
