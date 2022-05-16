7cae4dfba557fc845e9a5b12f3136bd4c4980f57
Mowalky <mowalky@outlook.com>
2022-05-16 11:47:58 -0500

    updated

--- src/calculator.js
+++
@@ -1,8 +0,0 @@
-export function add(num1,num2) {
-    return num1 + num2;
-}
-
-export default function abc() {
-    console.log('foo')
-    return 'bar';
-}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Header() {
+    return <header>This is the Header</header>
+}
+
+export default Header;
+
